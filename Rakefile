251bebc8484d74a661f9411e69b33ecf4fdd0bbc
Tibor Repasi <tibor.repasi@1und1.de>
2016-11-16 23:53:55 +0100

    ruby style update

--- Rakefile
+++ Rakefile
@@ -6,14 +6,14 @@ require 'puppet_blacksmith/rake_tasks'
 Rake::Task[:lint].clear
 PuppetLint.configuration.send('disable_variable_is_lowercase')
 PuppetLint::RakeTask.new :lint do |config|
-    config.log_format = "%{path}:%{linenumber}:%{check}:%{KIND}:%{message}"
-    config.fail_on_warnings = true
-    config.ignore_paths = [
-      "test/**/*.pp",
-      "vendor/**/*.pp",
-      "examples/**/*.pp",
-      "spec/**/*.pp",
-      "pkg/**/*.pp"
-    ]
-    config.disable_checks = ['class_inherits_from_params_class']
+  config.log_format = '%{path}:%{linenumber}:%{check}:%{KIND}:%{message}'
+  config.fail_on_warnings = true
+  config.ignore_paths = [
+    'test/**/*.pp',
+    'vendor/**/*.pp',
+    'examples/**/*.pp',
+    'spec/**/*.pp',
+    'pkg/**/*.pp'
+  ]
+  config.disable_checks = ['class_inherits_from_params_class']
 end
--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,30 +1,28 @@
 require 'spec_helper'
 describe 'demo' do
   context 'with default values for all parameters on debian' do
-    let :facts do {
-      :osfamily => 'Debian',
-    }
+    let :facts do
+      {
+        osfamily: 'Debian'
+      }
     end
-    it { 
+    it do
       should contain_class('demo')
       is_expected.to compile.with_all_deps
-      is_expected.to contain_package('ntp').with({
-        'ensure' => 'installed',
-      })
-    }
+      is_expected.to contain_package('ntp').with('ensure' => 'installed')
+    end
   end
 
   context 'with default values for all parameters on redhat' do
-    let :facts do {
-      :osfamily => 'Redhat',
-    }
+    let :facts do
+      {
+        osfamily: 'Redhat'
+      }
     end
-    it { 
+    it do
       should contain_class('demo')
       is_expected.to compile.with_all_deps
-      is_expected.to contain_package('ntpd').with({
-        'ensure' => 'installed',
-      })
-    }
+      is_expected.to contain_package('ntpd').with('ensure' => 'installed')
+    end
   end
 end
