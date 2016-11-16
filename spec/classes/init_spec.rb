ebd6485fac49dea132f03c6063dd9d923dbef556
Tibor Repasi <tibor.repasi@1und1.de>
2016-11-16 18:28:51 +0100

    add basic tests

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,6 +1,12 @@
 require 'spec_helper'
 describe 'demo' do
   context 'with default values for all parameters' do
-    it { should contain_class('demo') }
+    it { 
+      should contain_class('demo')
+      is_expected.to compile.with_all_deps
+      is_expected.to contain_package('ntp').with({
+        'ensure' => 'installed',
+      })
+    }
   end
 end
