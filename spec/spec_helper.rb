e59c738ad3a6cf2d3629ca642cbc0b9849ef5e36
Tibor Repasi <tibor.repasi@1und1.de>
2017-03-20 11:19:03 +0100

    coverage made parallel-aware

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,11 @@
+# First line of spec/spec_helper.rb
+require 'simplecov'
+
+SimpleCov.start do
+  add_filter '/spec/'
+  # Exclude bundled Gems in `/.vendor/`
+  add_filter '/.vendor/'
+end
+
 require 'puppetlabs_spec_helper/module_spec_helper'
+# Further content
